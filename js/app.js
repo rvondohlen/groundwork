71cb72e0683153e39785ab0121fbd902aeb1c221
rvondohlen <ryan@ryanvondohlen.com>
2014-06-05 09:12:03 -0400

    added back jquery dependecy and implemented smooth scrolling and -begin- button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 
 
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 // logic for getting current window size for title page scaling
 
 var groundwork = {};
